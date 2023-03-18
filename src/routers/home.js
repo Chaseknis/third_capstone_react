99715eaf0fdaad4ef6fc7fe649047f7870274da0
chaseknis <chase6th@gmail.com>
2023-03-18 18:59:24 +0200

    Add filter by city name option

--- src/routers/home.js
+++ src/routers/home.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { Link } from 'react-router-dom';
@@ -23,16 +23,26 @@ const Home = () => {
   const dispatch = useDispatch();
   const weatherData = useSelector(selectCitiesData);
 
+  const [filter, setFilter] = useState('');
+  const [filteredCities, setFilteredCities] = useState(cities);
+
   useEffect(() => {
     const fetchCitiesData = async () => {
-      await dispatch(fetchCities(cities));
+      dispatch(fetchCities(filteredCities));
     };
 
     fetchCitiesData();
-  }, [dispatch]);
+  }, [dispatch, filteredCities]);
 
   const getData = async (cityname) => {
     localStorage.setItem('cityname', JSON.stringify(cityname));
+  };
+
+  const handleFilterChange = (e) => {
+    const inputFilter = e.target.value.toLowerCase();
+    const filtered = cities.filter((city) => city.toLowerCase().includes(inputFilter));
+    setFilter(inputFilter);
+    setFilteredCities(filtered);
   };
 
   return (
@@ -42,6 +52,14 @@ const Home = () => {
           <h1>Weather App</h1>
         </div>
         <h2 className="small_title p-r-10">Forecast by City</h2>
+        <div className="filter_wrapper">
+          <input
+            type="text"
+            placeholder="Enter City Name"
+            value={filter}
+            onChange={handleFilterChange}
+          />
+        </div>
         <div className="cards_wrapper">
           {weatherData.map((weather, index) => (
             <Link to="/details" key={uuidv4()}>
@@ -49,15 +67,15 @@ const Home = () => {
                 className="card"
                 role="button"
                 tabIndex={0}
-                onClick={() => getData(weather.name)}
+                onClick={() => getData(filteredCities[index])}
                 onKeyDown={(e) => {
                   if (e.key === 'Enter' || e.key === ' ') {
-                    getData();
+                    getData(filteredCities[index]);
                   }
                 }}
                 style={{ color: 'black' }}
               >
-                <Card city={cities[index]} weather={weather} />
+                <Card city={filteredCities[index]} weather={weather} />
               </div>
             </Link>
           ))}
